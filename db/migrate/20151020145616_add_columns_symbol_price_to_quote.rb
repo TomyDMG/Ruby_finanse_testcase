f13dcd85498780804d0ff062499f10802bc96f93
M <tomydmg@gmail.com>
2015-10-23 12:58:11 +0300

    fix migrations

--- db/migrate/20151020145616_add_columns_symbol_price_to_quote.rb
+++ db/migrate/20151020145616_add_columns_symbol_price_to_quote.rb
@@ -1,7 +1,6 @@
-class AddColumnToQuotes < ActiveRecord::Migration
+class AddColumnsSymbolPriceToQuote < ActiveRecord::Migration
   def change
     add_column :quotes, :symbol, :string
     add_column :quotes, :price, :decimal
-    add_column :quotes, :date_trading, :datetime
   end
 end
